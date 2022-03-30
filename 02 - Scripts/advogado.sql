7631d5417b6f1a7c023cc8a4d6fd9d8c4750f508
Danilo Garcia <55713861+dantgarcia@users.noreply.github.com>
2022-03-30 11:55:22 -0300

    Add files via upload

---
+++ 02 - Scripts/advogado.sql
@@ -0,0 +1,8 @@
+create table cliente (
+	cod_adv INT PRIMARY KEY,
+	num_oab FOREIGN KEY,
+    nm_advogado VARCHAR(50),
+	gender VARCHAR(1),
+);
+insert into advogado (cod_adv, num_oab, nm_advogado, gender) values (1, '347.870 - RJ', 'Jorge Cleber', 'M');
+insert into advogado (cod_adv, num_oab, nm_advogado, gender) values (2, '381.668 - RJ', 'Jorge Cleber', 'M');
