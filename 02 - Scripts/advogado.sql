f03c5be01dab1d586ff440b5b7d04562152d290d
Danilo Garcia <55713861+dantgarcia@users.noreply.github.com>
2022-03-30 14:16:58 -0300

    Update advogado.sql

--- 02 - Scripts/advogado.sql
+++ 02 - Scripts/advogado.sql
@@ -1,8 +1,8 @@
 create table advogado (
 	cod_adv INT PRIMARY KEY,
-	num_oab FOREIGN KEY,
+	num_oab VARCHAR(12),
     nm_advogado VARCHAR(50),
-	gender VARCHAR(1),
+	gender VARCHAR(1)
 );
 insert into advogado (cod_adv, num_oab, nm_advogado, gender) values (1, '347.870 - RJ', 'Jorge Cleber', 'M');
 insert into advogado (cod_adv, num_oab, nm_advogado, gender) values (2, '381.668 - RJ', 'Bruno Aguiar', 'M');
